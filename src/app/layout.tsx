0a750b2e565ed8ee8b707c748984e3579295cc47
Omer Lugassy <omerlugassy2017@gmail.com>
2024-07-19 19:14:50 +0300

    rgx 3.0

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./styles/globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
+import { ModeToggle } from "@/components/mode-toggle";
 
 import {
   NavigationMenu,
@@ -14,6 +15,7 @@ import {
   NavigationMenuViewport,
 } from "@/components/ui/navigation-menu"
 import { navigationMenuTriggerStyle } from "@/components/ui/navigation-menu"
+import Link from "next/link";
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -31,16 +33,16 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-      
+      <ModeToggle/>
       <NavigationMenu>
         <NavigationMenuList>
-          <NavigationMenuItem>
-            
-            <NavigationMenuTrigger>Item 1</NavigationMenuTrigger>
-            <NavigationMenuContent>
-                <NavigationMenuLink>Sign</NavigationMenuLink>
-            </NavigationMenuContent>
-          </NavigationMenuItem>
+        <NavigationMenuItem>
+        <Link href="components/sign-in" legacyBehavior passHref>
+          <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+            Sign In
+          </NavigationMenuLink>
+        </Link>
+        </NavigationMenuItem>
         </NavigationMenuList>
       </NavigationMenu>
       
